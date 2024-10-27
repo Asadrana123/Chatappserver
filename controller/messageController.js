aa1ce0e8d06bd4ddb4872c2b6fb5dabd1a5a0bf8
Asad Rana <asadlukman246@gmail.com>
2024-10-27 10:32:40 +0530

    edits

--- controller/messageController.js
+++ controller/messageController.js
@@ -6,8 +6,6 @@ exports.sendMessageToAIController = async (req, res) => {
   const chatId = req.body.chatId;
   const message = req.body.content;
   const sender = req.user;
-  var normalMessage;
-  var aiMessage;
   console.log(sender);
   try {
     const result = await messageModel.create({
@@ -15,7 +13,7 @@ exports.sendMessageToAIController = async (req, res) => {
       content: message,
       sender: sender,
     })
-     normalMessage = await messageModel.find({
+     const data = await messageModel.find({
       _id: result._id
     }).populate({
       path: "chat",
@@ -33,21 +31,24 @@ exports.sendMessageToAIController = async (req, res) => {
         path: "sender"
       }
     });
+    return res.status(200).json({ data });
   } catch (err) {
     console.log(err);
     return res.status(400).json({ err });
   }
+};
+exports.getMessagefromAIcontrolller=async(req,res)=>{
+  const chatId = req.body.chatId;
   try{
     const genAI = new GoogleGenerativeAI(process.env.API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-    const airesult = await model.generateContent(message);
-    try {
-      const result = await messageModel.create({
+    const airesult = await model.generateContent(req.body.content);
+    const result = await messageModel.create({
         chat: chatId,
         content: airesult.response.text(),
         sender: "67189a1916769aad12310513",
       })
-       aiMessage = await messageModel.find({
+       data = await messageModel.find({
         _id: result._id
       }).populate({
         path: "chat",
@@ -65,15 +66,12 @@ exports.sendMessageToAIController = async (req, res) => {
           path: "sender"
         }
       });
-      return res.status(200).json([normalMessage,aiMessage]);
+      return res.status(200).json({data});
     } catch (err) {
       console.log(err);
       return res.status(400).json({ err });
     }
-  }catch(error){
-         console.log(error);
-  }
-};
+}
 exports.sendMessage = async (req, res) => {
   const chatId = req.body.chatId;
   const message = req.body.content;
--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const auth = require("../middleware/auth");
-const {sendMessage,allMessage, sendMessageToAIController}=require("../controller/messageController")
+const {sendMessage,allMessage, sendMessageToAIController,getMessagefromAIcontrolller}=require("../controller/messageController")
 const router = express.Router();
 router.post('/sendMessage',auth,sendMessage);
 router.get("/:chatId", auth, allMessage);
-router.post("/aichat",auth,sendMessageToAIController);
+router.post("/send-chatbot",auth,sendMessageToAIController);
+router.post("/get-chatbot",auth,getMessagefromAIcontrolller);
 module.exports=router;
